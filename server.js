2a6e4eca9ba02ff273b0e14fc79890dde2f706bd
Eric Stephenson <ericb.stephenson@gmail.com>
2019-03-10 09:26:06 -0700

    Add docker-compose.yml, Install pg

--- server.js
+++ server.js
@@ -2,16 +2,22 @@
 
 'use strict';
 
-const express = require('express');
-
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+// Packages
+const express = require('express');
+const pg = require('pg');
+
+// Database
+// pg.connect('postgres://postgres:password@localhost:5432/practice-docker');
+
 // App
 const app = express();
+
 app.get('/', (req, res) => {
-  res.send('Hello world\n')
+  res.status(200).send('Hello World\n');
 });
 
 app.listen(PORT, HOST);
